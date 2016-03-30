8e288604b99cf839f5c483f158cbe6cfee430a94
TheGreatMonkey <TheGreatMonkey68@gmail.com>
2016-03-30 08:33:44 -0700

    Setup Supply and Sell Vehicle points

--- server/CfgFunctions.hpp
+++ server/CfgFunctions.hpp
@@ -10,4 +10,6 @@ class ServerDir
 	class setupStoreNPC {file = "server\functions\setupStoreNPC.sqf";};
 	class setupMissionObject {file = "server\functions\setupMissionObject.sqf";};
 	class setupMissionATM {file = "server\functions\setupMissionATM.sqf";};
+	class setupResupplyTruck {file = "server\functions\setupResupplyTruck.sqf";};
+	class setupSellTruck {file = "server\functions\setupSellTruck.sqf";};
 };
