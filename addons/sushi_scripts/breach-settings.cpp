53dab6c8d23f451c339c7db124429837736936bb
TheGreatMonkey <TheGreatMonkey68@gmail.com>
2016-10-26 19:41:31 -0700

    Sushi

--- addons/sushi_scripts/breach-settings.cpp
+++ addons/sushi_scripts/breach-settings.cpp
@@ -24,9 +24,9 @@ class BREACH
 			/*Zone example:*/
 			class zone_1 //< custom zone name
 			{
-				zone = "doorlockall1"; //< marker (rectangle or elipse) that covers zone.  Change this name to the one you made
+				zone = "Breach"; //< marker (rectangle or elipse) that covers zone.  Change this name to the one you made
 				closeDoors = 1; //<1- close all doors in zone, 0- do nothing
-				lockAll = 1; //< 1- lock all closed doors in zone, 0-lock random (50%) closed doors, 2-lock 75% of doors in the zone
+				lockAll = 0; //< 1- lock all closed doors in zone, 0-lock random (50%) closed doors, 2-lock 75% of doors in the zone
 			};
 		};
 	};
